9dfd823378a1906862e004c9aeed5238486ab678
mak-454 <ahmed.khan@oneconvergence.com>
2019-09-18 18:20:47 +0530

    renamed dkube to minio, modified logic to be generic by removing any reference to dkube

--- github-issue-classification/minio/upload.py
+++ github-issue-classification/minio/upload.py
@@ -1,13 +1,21 @@
 from minio import Minio
 from minio.error import ResponseError
 
+import logging
+
 import os
+import re
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 client = Minio(os.getenv("S3_ENDPOINT"),
                access_key=os.getenv("AWS_ACCESS_KEY_ID"),
                secret_key=os.getenv("AWS_SECRET_ACCESS_KEY"), secure=False)
 
-prefix = os.getenv('OUT_DIR').replace("s3://dkube/",'')
+bucket = os.getenv("S3_BUCKET")
+outdir = os.getenv('OUT_DIR')
+prefix = re.sub("s3://%s/"%(bucket), '', outdir)
 
 def upload_objects():
 
@@ -18,10 +26,8 @@ def upload_objects():
             path = path.replace("\\","/")
             directory_name = path.replace(root_path,"")
             for file in files:
-                client.fput_object(os.getenv("S3_BUCKET"), prefix + '/' + directory_name+'/'+file, os.path.join(path, file), 'text/plain')
+                client.fput_object(bucket, prefix + '/' + directory_name+'/'+file, os.path.join(path, file), 'text/plain')
     except Exception as err:
-        print(err)
-
+        logger.exception(err)
 if __name__ == '__main__':
     upload_objects()
-
