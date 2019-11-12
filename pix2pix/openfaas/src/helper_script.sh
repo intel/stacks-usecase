62ebb809a29055ab53d524f70d77670bd6fa73e2
Daniela Plascencia <daniela.plascencia@intel.com>
2019-11-12 11:47:11 -0600

    Updates helper_script.sh with right links

--- pix2pix/openfaas/src/helper_script.sh
+++ pix2pix/openfaas/src/helper_script.sh
@@ -1,16 +1,16 @@
 #! /usr/bin/env bash
 
 # Clone usecases repo and cd into pix2pix
-git clone https://github.intel.com/verticals/usecases
-cp -a ./usecases/tensorflow/pix2pix/infer.py ./infer.py \
-&& cp -a ./usecases/tensorflow/pix2pix/scripts .
+git clone https://github.com/intel/stacks-usecase.git
+cp -a ./stacks-usecase/pix2pix/infer.py ./infer.py \
+&& cp -a ./stacks-usecase/pix2pix/scripts .
 
 # Save a pretrained model inside model/
 mkdir -p ./models
 mv generator_model.h5 ./models/
 
 # Workdir cleanup
-rm -rf ./usecases/ \
+rm -rf ./stacks-usecase/ \
 requirements.txt \
 helper_script.sh \
 main.py
