522ffe93e079f6cb72d54b4fc6fbc62bffc7af9d
Rahul <rahul.unnikrishnan.nair@intel.com>
2019-11-15 15:00:14 -0800

    Update run.sh

--- .ci/run.sh
+++ .ci/run.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
 set -e
+rm -rf pages
 
+# HTTP Service
+mkdir -p docs/plugins/service/
+rm -f docs/plugins/service/http
+ln -s "${PWD}/service/http/docs/" docs/plugins/service/http
+
+# generate docs
 sphinx-build -b html docs pages
+find pages/ -name \*.html -exec \
+  sed -i 's/<span class="gp">\&gt;\&gt;\&gt; <\/span>//g' {} \;
+find pages/ -name \*.html -exec \
+  sed -i 's/<span class="go">\&gt;\&gt;\&gt;<\/span>//g' {} \;
+cp -r docs/images pages/
+touch pages/.nojekyll
